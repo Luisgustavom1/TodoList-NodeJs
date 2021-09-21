bbd51ba25da0acd8e253c4e1fc50d758042a0ddb
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-09-20 23:43:31 -0300

    Finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,79 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find(user => user.username === username);
+
+  if(!userExists) {
+    return response.status(404).json({
+      error: 'Usuario não encontrado'
+    });
+  };
+
+  request.user = userExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: 'Todos esgotadas'
+    });
+  };
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const userExists = users.find(user => user.username === username);
+
+  if(!userExists) {
+    return response.status(404).json({
+      error: 'User not found'
+    });
+  };
+
+  const isUuid = validate(id);
+
+  if(!isUuid) {
+    return response.status(400).json({
+      error: 'Id invalid'
+    });
+  };
+
+  const todoExists = userExists.todos.find(todo => todo.id === id);
+
+  if(!todoExists) {
+    return response.status(404).json({
+      error: 'Id not found'
+    });
+  };
+
+  request.user = userExists;
+  request.todo = todoExists;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userExists = users.find(user => user.id === id);
+
+  if(!userExists) {
+    return response.status(404).json({
+      error: 'User not found'
+    });
+  };
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
